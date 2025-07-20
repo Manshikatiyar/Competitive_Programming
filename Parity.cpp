95a2be14cb708943dcdd62537ba5efcd7fa79cf6
Manshikatiyar <nishukatiyar97@gmail.com>
2025-07-20 21:40:07 +0530

    15 july

---
+++ Parity.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	for( int i=0;i<t;i++){
+	    int x;
+	    cin>>x;
+	    if(x%2==0){
+	        cout<<"Yes"<<endl;
+	        
+	    }else{
+	        cout<<"No"<<endl;}
+	        
+	    }
+	}
+
+    
+	
+
+
+
+
+	
